79712717863996b31e73fde53cd11b7d2237c5ae
Sam Cook <WhenPotatoesFry@gmail.com>
2019-06-14 12:37:13 -0700

    clean up js and add more detail to js comments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,27 @@
 $(document).ready(function() {
+  // Gets user input. Builds a string of numbers from 0 counting up to user specified number. Replaces all numbers containing 1, 2, or 3 with specified text. Returns a string.
   $("button").click(function() {
     //get user textInput and store only integers
     var userInput = parseInt($("input:text[name=numberInput]").val());
-    console.log(userInput);
-
+    //Builds a string of numbers from 0 counting up to user specified number
     var workingArray = createRange(userInput);
-    console.log(workingArray);
+    // find all numbers containing 3's and replace all numbers containing 3's with "I'm sorry Dave, I can't do that."
     workingArray = findThrees(workingArray);
-    console.log(workingArray);
+    //find all the numbers containing 2's and replace 2's with "Boop"
     workingArray = findTwos(workingArray);
-    console.log(workingArray);
+    //find all the numbers containing 1's and replace 1's with "Beep"
     workingArray = findOnes(workingArray);
-    console.log(workingArray);
+    // join all elements of array into a string with spaces between each element
     userInput = arrayToStringWithSpaces(workingArray);
-    console.log(userInput);
-    // output final string to webpage
+    // output final string
     $("#result").text(userInput);
     //create a range of numbers between 0 and numberInput
     function createRange(numberInput) {
       var numberRangeArray = [];
-      for (i = 0; i < userInput; i++) {
+      for (i = 0; i <= userInput; i++) {
         numberRangeArray.push(i);
       }
-      numberRangeArray.push(numberInput);
-      return numberRangeArray
+      return numberRangeArray;
     }
     // find all numbers containing 3's and replace all numbers containing 3's with "I'm sorry Dave, I can't do that."
     function findThrees(arrayOfNumbers) {
@@ -52,7 +50,7 @@ $(document).ready(function() {
       }
       return arrayOfNumbers;
     }
-    // concat into a string with spaces between each element
+    // join all elements of array into a string with spaces between each element
     function arrayToStringWithSpaces(array) {
       return array.join(" ");
     }
