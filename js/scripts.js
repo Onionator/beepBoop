3d69036b8d3d44ab3162051f6f0ae6f6dc2ed1ef
Sam Cook <WhenPotatoesFry@gmail.com>
2019-06-14 10:11:59 -0700

    create basic html and start outlining js.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    //get user textInput
+
+    // check to see if user input is in numbers. if not then output an error message
+
+    //create a range of numbers between 0 and userInput
+
+    //find all the numbers containing 1's
+
+    // replace 1's with "Beep"
+
+    //find all the numbers containing 2's
+
+    // replace 2's with "Boop"
+
+    // find all numbers containing 3's
+
+    //replace all numbers containing 3's with "I'm sorry Dave, I can't do that."
+
+    //
+  });
+});
