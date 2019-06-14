0f05f6aec08510066a590deab509f7885fbfdbc7
Sam Cook <WhenPotatoesFry@gmail.com>
2019-06-14 11:54:25 -0700

    test out code to find a 3 in a number

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
     var userInput = parseInt($("input:text[name=numberInput]").val());
     console.log(userInput);
 
-    userInput = createRange(userInput);
-    console.log(userInput);
+    var workingArray = createRange(userInput);
+    console.log(workingArray);
+    workingArray = findThrees(workingArray);
+    //console.log(workingArray);
+
 
     //create a range of numbers between 0 and numberInput
     function createRange(numberInput) {
@@ -14,10 +17,28 @@ $(document).ready(function() {
         numberRangeArray.push(i);
       }
       numberRangeArray.push(numberInput);
-      return numberRangeArray.join(" ");
+      return numberRangeArray
     }
 
     // find all numbers containing 3's
+    function findThrees(arrayOfNumbers) {
+      var three = ["3"];
+      for (i = 0; i < arrayOfNumbers.length; i++) {
+        console.log( three.includes(arrayOfNumbers[i].toString()));
+
+        //console.log(three.includes(parseStr(arrayOfNumbers[i]).split("")));
+        // if (arrayOfNumbers.filter(3)) {
+        //   console.log("found a three")
+        // }
+        // if (arrayOfNumbers.every(3)) {
+        //   console.log(arrayOfNumbers[i]);
+        //   arrayOfNumbers[i] = "I'm sorry Dave, I can't do that."
+        // } else {
+        //   console.log("not a three");
+        // }
+
+      }
+    }
 
     //replace all numbers containing 3's with "I'm sorry Dave, I can't do that."
 
