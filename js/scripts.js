f4661276096e9013b10dca75c27842f930d0703c
Sam Cook <WhenPotatoesFry@gmail.com>
2019-06-14 10:58:26 -0700

    create a js function to create a range of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,36 @@
 $(document).ready(function() {
   $("button").click(function() {
-    //get user textInput
+    //get user textInput and store only integers
+    var userInput = parseInt($("input:text[name=numberInput]").val());
+    console.log(userInput);
 
-    // check to see if user input is in numbers. if not then output an error message
+    userInput = createRange(userInput);
+    console.log(userInput);
 
-    //create a range of numbers between 0 and userInput
-
-    //find all the numbers containing 1's
-
-    // replace 1's with "Beep"
-
-    //find all the numbers containing 2's
-
-    // replace 2's with "Boop"
+    //create a range of numbers between 0 and numberInput
+    function createRange(numberInput) {
+      var numberRangeArray = [];
+      for (i = 0; i < userInput; i++) {
+        numberRangeArray.push(i);
+      }
+      numberRangeArray.push(numberInput);
+      return numberRangeArray.join(" ");
+    }
 
     // find all numbers containing 3's
 
     //replace all numbers containing 3's with "I'm sorry Dave, I can't do that."
 
-    //
+    //find all the numbers containing 2's
+
+    // replace 2's with "Boop"
+
+    //find all the numbers containing 1's
+
+    // replace 1's with "Beep"
+
+    // concat into a string
+
+    // output final string to webpage
   });
 });
